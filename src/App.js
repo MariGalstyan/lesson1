1538c4886dd63357a759249ecb352733be2697a4
MariGalstyan <galstyanmari1996@gmail.com>
2021-02-02 23:44:22 +0400

    lesson3

--- src/App.js
+++ src/App.js
@@ -3,24 +3,12 @@ import './App.css';
 import Red from './colors/Red';
 import Green from './colors/Green';
 import Blue from './colors/Blue';
+import SellOnline from './SellOnline/SellOnline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sell_wrapper">
+      <SellOnline />
     </div>
   );
 }
---
+++ src/SellOnline/SellOnline.js
@@ -0,0 +1,9 @@
+export default function SellOnline() {
+    return (
+        <div className="SellOnline">
+            <img src="https://images.ctfassets.net/2d5q1td6cyxq/4j8fYE0CQJXa7ncvN4rqfi/51ab3c595af2d9875b728a1978a9795d/cart.svg" alt="sell-pic" />
+            <h2>Start selling online</h2>
+            <p>Easily sell online and ship orders to customers, offer curbside pickup, or local delivery—no matter what type of business you have. Plus, sell on Instagram, Facebook, and more. </p>
+        </div>
+    )
+}
